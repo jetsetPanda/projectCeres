fdd71b6872742728a36024c9eebd4224d7c534af
jetsetPanda <44765030+jetsetPanda@users.noreply.github.com>
2020-07-29 15:32:39 -0400

    styled component Header

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,37 @@
 import React from 'react';
-import styles from "../styles/Header.module.css"
+import styled from "styled-components";
+
+// styled components
+const Container = styled.div`
+    margin-top: 20px;
+    align-items: center;
+    max-width: 1280px;
+    margin: 0 auto;
+`
+
+const HeaderText = styled.h1`
+    font-size: 40px;
+    line-height: 47px;
+    color: #222222;
+`
+
+const HeaderCaption = styled.p`
+    max-width: 900px;
+    font-size: 20px;
+    line-height: 23px;
+    color: $333333;
+`
 
 const Header = () => {
     return (
         <React.Fragment>
-            <div className="container header">
-                <h1>Appliance Packages</h1>
-                <p>Looking for a great deal on home appliances? Packages are the best bet. Most appliance packages consist of a range, refrigerator, over-the-range microwave, and dishwasher. However, some upscale brands have packages that incorporate wall ovens, cooktops, and integrated refrigerators. </p>
+            <div className="container">
+                <Container>
+                    <HeaderText>Appliance Packages</HeaderText>
+                    <HeaderCaption>Looking for a great deal on home appliances? Packages are the best bet. Most appliance packages consist of a range, refrigerator, over-the-range microwave, and dishwasher. However, some upscale brands have packages that incorporate wall ovens, cooktops, and integrated refrigerators. </HeaderCaption>
+                </Container>
             </div>
+
         </React.Fragment>
 
     );
