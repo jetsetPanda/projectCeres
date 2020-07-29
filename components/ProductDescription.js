0f7c3d74d21b4a6c5df59b777cd4056dc5a71c91
jetsetPanda <44765030+jetsetPanda@users.noreply.github.com>
2020-07-29 12:17:25 -0400

    append ellipses logic to concat text

--- components/ProductDescription.js
+++ components/ProductDescription.js
@@ -1,10 +1,12 @@
 
 export default function ProductDescription(props){
-    let {series, description} = props.data;
+    let {brand, series, description} = props.data;
 
     let text = ` ${series} ${description}`;
 
+    let ellipsis = (text.length >= 104) ? '...' : '';
+
     return (
-        `${text.substr(0, 105)}`
+        `${text.substr(0, 104)}${ellipsis}`
     )
 };
