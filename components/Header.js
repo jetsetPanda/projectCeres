25fb920879f288be4a6dc509c015637e6fdef2ec
jetsetPanda <44765030+jetsetPanda@users.noreply.github.com>
2020-08-01 20:11:34 -0400

    using constants for css props in styled-components

--- components/Header.js
+++ components/Header.js
@@ -10,34 +10,47 @@ const Container = styled.div`
 `
 
 const HeaderText = styled.h1`
-    font-size: ${props => props.size};
-    line-height: 47px;
+    font-size: ${props => props.style.h1.size};
+    line-height: ${props => props.style.h1.lineH};
     color: #222222;
 `
 
 const HeaderCaption = styled.p`
     max-width: 900px;
-    font-size: ${props => props.size};
-    line-height: 23px;
+    font-size: ${props => props.style.p.size};
+    line-height: ${props => props.style.p.lineH};
     color: #333333;
 `
 
 const Header = (props) => {
 
-    const fontSizes = (props.viewport === 'mobile') ? {
-        h1 : '25px',
-        p : '12px'
+    const styles = (props.viewport === 'mobile') ? {
+        h1: {
+            size: '25px',
+            lineH: '29px'
+        },
+        p: {
+            size: '12px',
+            lineH: '15px'
+        }
+
     } : {
-        h1 : '40px',
-        p : '20px'
+        h1: {
+            size: '40px',
+            lineH: '47px'
+        },
+        p: {
+            size: '20px',
+            lineH: '23px'
+        }
     };
 
     return (
         <React.Fragment>
             <div className="container">
                 <Container>
-                    <HeaderText size={fontSizes.h1}>Appliance Packages</HeaderText>
-                    <HeaderCaption size={fontSizes.p}>Looking for a great deal on home appliances? Packages are the best bet. Most appliance packages consist of a range, refrigerator, over-the-range microwave, and dishwasher. However, some upscale brands have packages that incorporate wall ovens, cooktops, and integrated refrigerators. </HeaderCaption>
+                    <HeaderText style={styles}>Appliance Packages</HeaderText>
+                    <HeaderCaption style={styles}>Looking for a great deal on home appliances? Packages are the best bet. Most appliance packages consist of a range, refrigerator, over-the-range microwave, and dishwasher. However, some upscale brands have packages that incorporate wall ovens, cooktops, and integrated refrigerators. </HeaderCaption>
                 </Container>
             </div>
 
